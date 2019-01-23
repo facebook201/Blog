d2cf64babcf44224800efb300adc0f75c1fdd943
syo <syolmm@qq.com>
2019-01-23 09:56:07 +0800

    add: 增加按位操作符

---
+++ 算法/al.js
@@ -0,0 +1,17 @@
+
+// 每个节点对象
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// 二叉搜索树
+class BST {
+  constructor() {
+    this.root = null;
+    this.size = 0;
+  }
+}
